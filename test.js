a2968e72f55a80a315da321079c98293e75211bb
lyquocnam <lyquocnam@live.com>
2017-11-01 08:36:53 +0700

    update test

---
+++ test.js
@@ -0,0 +1,79 @@
+const t = require('tap')
+const test = t.test
+const Fastify = require('fastify')
+const fastifySequelize = require('fastify-sequelize')
+const { resolve } = require('path')
+const Sequelize = require('sequelize')
+
+test('fastify.sequelize should exist', t => {
+    t.plan(2)
+
+    const fastify = Fastify()
+
+    fastify.register(fastifySequelize, {
+        dialect: 'sqlite',
+        storage: resolve(__dirname, 'db.sqlite')
+    })
+
+    fastify.ready(err => {
+        t.error(err)
+        t.ok(fastify.sequelize)
+
+        fastify.sequelize.close()
+        fastify.close()
+    })
+})
+
+test('Sequelize instance name should work', t => {
+    t.plan(2)
+
+    const fastify = Fastify()
+
+    fastify.register(fastifySequelize, {
+        instance: 'db', // instance name
+        dialect: 'sqlite',
+        storage: resolve(__dirname, 'db.sqlite')
+    })
+
+    fastify.ready(err => {
+        t.error(err)
+        t.ok(fastify.db) // instance name
+        
+        fastify.db.close()
+        fastify.close()
+    })
+})
+
+test('Sequelize should work fine with records', t => {
+    t.plan(3)
+
+    const fastify = Fastify()
+
+    fastify.register(fastifySequelize, {
+        dialect: 'sqlite',
+        storage: resolve(__dirname, 'db.sqlite')
+    })
+
+    fastify.ready(err => {
+        t.error(err)
+        t.ok(fastify.sequelize)
+
+        const User = fastify.sequelize.define('user', {
+            name: Sequelize.STRING
+        })
+
+        User.sync({ force: true }).then(() => {
+            User.create({ name: 'Iron Man' })
+                .then(user => {
+                    t.ok(user)
+                })
+                .catch(err => {
+                    t.error(err)
+                })
+        })
+        .catch(err => t.error(err))
+
+        fastify.close()
+    })
+})
+
